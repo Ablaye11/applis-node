e19aeb4d4f84fe54e4f0d04a84d38c30edb4f77b
ablaye seck <seckablaye519@gmail.com>
2025-03-12 11:36:35 +0000

    new essai

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+
 
 const express = require('express');
 const mysql = require('mysql2');
@@ -12,7 +12,7 @@ app.set('views', './views');
 // Middleware pour parser les données du formulaire
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+require('dotenv').config();
 // Connexion à la base de données MySQL
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
